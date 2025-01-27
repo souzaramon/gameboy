c049da7732dbe6b164b5dbf950818a35de64f81d
souzaramon.dev <souzaramon.dev@gmail.com>
2025-01-27 19:51:59 -0300

    test: improve utils tests readability

--- pkg/sm83/utils_test.go
+++ pkg/sm83/utils_test.go
@@ -3,35 +3,66 @@ package sm83
 import "testing"
 
 func TestGetNth(t *testing.T) {
-	cases := [][3]interface{}{
-		// byte, nth, expected
-		{byte(0xFF), 1, true},
-		{byte(0x00), 1, false},
+	cases := []struct {
+		name     string
+		i        int
+		value    byte
+		expected bool
+	}{
+		{
+			name:     "Get bit at 7",
+			i:        7,
+			value:    0b10000000,
+			expected: true,
+		},
+		{
+
+			name:     "Get bit at 6",
+			i:        6,
+			value:    0b10000000,
+			expected: false,
+		},
 	}
 
 	for _, c := range cases {
-		data := GetNthBit(c[0].(byte), c[1].(int))
-		expected := c[2].(bool)
+		data := GetNthBit(c.value, c.i)
 
-		if data != expected {
-			t.Fatalf(`GetNthBit(%#v, %d) = %t, want %t`, c[0], c[1], data, expected)
+		if data != c.expected {
+			t.Fatalf(`GetNthBit(%b, %d) = %t, want %t`, c.value, c.i, data, c.expected)
 		}
 	}
 }
 
 func TestSetNth(t *testing.T) {
-	cases := [][4]interface{}{
-		// byte, nth, value, expected
-		{byte(0x00), 7, true, byte(0x80)},
-		{byte(0x80), 7, false, byte(0x00)},
+	cases := []struct {
+		name     string
+		i        int
+		v        bool
+		value    byte
+		expected byte
+	}{
+		{
+			name:     "Set bit at 0",
+			i:        0,
+			v:        true,
+			value:    0b10000000,
+			expected: 0b10000001,
+		},
+		{
+
+			name:     "Set bit at 1",
+			i:        1,
+			v:        false,
+			value:    0b10000010,
+			expected: 0b10000000,
+		},
 	}
 
 	for _, c := range cases {
-		data := SetNthBit(c[0].(byte), c[1].(int), c[2].(bool))
-		expected := c[3].(byte)
+		data := SetNthBit(c.value, c.i, c.v)
 
-		if data != expected {
-			t.Fatalf(`SetNthBit(%#v, %d, %t) = %#v, want %#v`, c[0], c[1], c[2], data, expected)
+		if data != c.expected {
+			t.Fatalf(`SetNthBit(%b, %d, %t) = %b, want %b`, c.value, c.i, c.v, data, c.expected)
 		}
 	}
 }
