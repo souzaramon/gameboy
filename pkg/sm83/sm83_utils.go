3a4b0270bd79c99db4c866deb90ebc2b1f1d0a19
souzaramon.dev <souzaramon.dev@gmail.com>
2025-01-27 18:44:04 -0300

    feat: implement GetNthBit and SetNthBit utils

---
+++ pkg/sm83/sm83_utils.go
@@ -0,0 +1,13 @@
+package sm83
+
+func GetNthBit(b byte, n int) bool {
+	return b&(byte(1)<<n) != 0
+}
+
+func SetNthBit(b byte, n int, v bool) byte {
+	if v {
+		return b | (byte(1) << n)
+	} else {
+		return b & ^(byte(1) << n)
+	}
+}
