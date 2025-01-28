5d965e248cc1f17a2472b7511de5555ff091e514
souzaramon.dev <souzaramon.dev@gmail.com>
2025-01-28 00:45:27 -0300

    feat: add missing InstructionKind

--- pkg/sm83/instruction.go
+++ pkg/sm83/instruction.go
@@ -12,7 +12,20 @@ type RegisterKind string
 
 const (
 	RK_NONE RegisterKind = "NONE"
-	RK_HL   RegisterKind = "HL"
+	RK_A    RegisterKind = "RK_A"
+	RK_F    RegisterKind = "RK_F"
+	RK_B    RegisterKind = "RK_B"
+	RK_C    RegisterKind = "RK_C"
+	RK_D    RegisterKind = "RK_D"
+	RK_E    RegisterKind = "RK_E"
+	RK_H    RegisterKind = "RK_H"
+	RK_L    RegisterKind = "RK_L"
+	RK_AF   RegisterKind = "RK_AF"
+	RK_BC   RegisterKind = "RK_BC"
+	RK_DE   RegisterKind = "RK_DE"
+	RK_HL   RegisterKind = "RK_HL"
+	RK_SP   RegisterKind = "RK_SP"
+	RK_PC   RegisterKind = "RK_PC"
 )
 
 type InstructionKind string
