4c31f7718816ecf2b7077b3b713e732de48aafdc
souzaramon.dev <hi.souzaramon@gmail.com>
2025-02-02 03:39:03 -0300

    refactor: split step

--- LR35902_instructions.go
+++ LR35902_instructions.go
@@ -1,48 +1,48 @@
 package LR35902
 
 func (cpu *CPU) LD_r8_r8(r1 string, r2 string) {
-	cpu.Registers.SetR8(r1, cpu.Registers.GetR8(r2))
+	cpu.SetRegister8(r1, cpu.GetRegister8(r2))
 }
 
 func (cpu *CPU) LD_r16_r16(r1 string, r2 string) {
-	cpu.Registers.SetR16(r1, cpu.Registers.GetR16(r2))
+	cpu.SetRegister16(r1, cpu.GetRegister16(r2))
 }
 
 func (cpu *CPU) LD_r8_n8(r1 string) {
-	cpu.Registers.SetR8(r1, cpu.Memory.Read8(cpu.Registers.PC))
+	cpu.SetRegister8(r1, cpu.Memory.Read8(cpu.Registers.PC))
 	cpu.Registers.PC += 1
 	cpu.Cycles += 4
 }
 
 func (cpu *CPU) LD_r16_n16(r1 string) {
-	cpu.Registers.SetR16(r1, cpu.Memory.Read16(cpu.Registers.PC))
+	cpu.SetRegister16(r1, cpu.Memory.Read16(cpu.Registers.PC))
 	cpu.Registers.PC += 2
 	cpu.Cycles += 8
 }
 
 func (cpu *CPU) LD_r16_A(r1 string) {
-	addr := cpu.Registers.GetR16(r1)
+	addr := cpu.GetRegister16(r1)
 
 	if r1 == R_C {
 		addr |= 0xFF00
 	}
 
-	cpu.Memory.Write8(addr, cpu.Registers.GetR8(R_A))
+	cpu.Memory.Write8(addr, cpu.GetRegister8(R_A))
 }
 
 func (cpu *CPU) LD_HL_n8(r2 string) {
-	cpu.Memory.Write8(cpu.Registers.GetR16(R_HL), cpu.Registers.GetR8(r2))
+	cpu.Memory.Write8(cpu.GetRegister16(R_HL), cpu.GetRegister8(r2))
 }
 
 func (cpu *CPU) LD_SP_n16() {
-	cpu.Registers.SetR16(R_SP, cpu.Memory.Read16(cpu.Registers.PC))
+	cpu.SetRegister16(R_SP, cpu.Memory.Read16(cpu.Registers.PC))
 	cpu.Registers.PC += 2
 }
 
 func (cpu *CPU) LD_A_r16(r2 string) {
-	cpu.Registers.SetR8(R_A, cpu.Memory.Read8(cpu.Registers.GetR16(r2)))
+	cpu.SetRegister8(R_A, cpu.Memory.Read8(cpu.GetRegister16(r2)))
 }
 
 func (cpu *CPU) LD_r8_HL(r1 string) {
-	cpu.Registers.SetR8(r1, cpu.Memory.Read8(cpu.Registers.GetR16(R_HL)))
+	cpu.SetRegister8(r1, cpu.Memory.Read8(cpu.GetRegister16(R_HL)))
 }
